c2af5b03d60fbd40417ad8eb847f5dc7dbb6add0
Ratson <github@ratson.name>
2016-11-19 21:07:42 +0800

    Auto-config for babel-plugin-dev-expression

--- packages/eslint-config-concise/src/index.js
+++ packages/eslint-config-concise/src/index.js
@@ -1,9 +1,15 @@
-module.exports = {
+/* eslint-disable dot-notation */
+import _ from 'lodash'
+import readPkgUp from 'read-pkg-up'
+
+const config = {
+  env: {},
   extends: [
     'eslint-config-airbnb-base',
     'eslint-config-xo/esnext',
     'eslint-config-concise-style',
   ].map(require.resolve),
+  globals: {},
   plugins: [
     'filenames',
     'promise',
@@ -24,3 +30,12 @@ module.exports = {
     'unicorn/filename-case': 'off',
   },
 }
+
+const {pkg} = readPkgUp.sync()
+const deps = _.reduce(_.pick(pkg, ['dependencies', 'devDependencies', 'peerDependencies', 'optionalDependencies']), (r, o) => Object.assign(r, o), {})
+
+if (deps['babel-plugin-dev-expression']) {
+  config.globals['__DEV__'] = true
+}
+
+module.exports = config
