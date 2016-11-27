8271a0ca288eeb03fd881ec188ff33d97f97ff15
Ratson <github@ratson.name>
2016-11-27 08:28:33 +0800

    Add init command

---
+++ packages/csc/src/commands/init.js
@@ -0,0 +1,62 @@
+import {exec} from 'child_process'
+
+import _ from 'lodash'
+import inquirer from 'inquirer'
+
+export const desc = 'Run config initialization wizard'
+
+function getPeerDependencies(pkg) {
+  switch (pkg) {
+    case 'eslint-config-concise':
+      return [
+        'eslint',
+        'eslint-plugin-eslint-comments',
+        'eslint-plugin-html',
+        'eslint-plugin-markdown',
+        'eslint-plugin-node',
+        'eslint-plugin-promise',
+      ]
+    case 'eslint-config-concise-react':
+      return [
+        'eslint',
+        'eslint-plugin-react',
+      ]
+    case 'eslint-config-control-freak':
+      return getPeerDependencies('eslint-config-concise').concat([
+        'eslint-plugin-filenames',
+        'eslint-plugin-import',
+      ])
+  }
+  return []
+}
+
+export function handler(argv) {
+  const ui = new inquirer.ui.BottomBar()
+  inquirer.prompt([
+    {
+      type: 'checkbox',
+      name: 'packages',
+      message: 'Which packages do you want to install?',
+      default: ['eslint-config-concise'],
+      choices: [
+        'csc',
+        'eslint-config-concise',
+        'eslint-config-concise-react',
+        'eslint-config-concise-style',
+        'eslint-config-control-freak',
+      ],
+    },
+  ])
+  .then((answers) => {
+    const installPackages = _.uniq(
+      _.flatMap(answers.packages, pkg => getPeerDependencies(pkg))
+      .concat(answers.packages))
+    const installCmd = installPackages.reduce(
+      (r, pkg) => `${r} ${pkg}`,
+      'npm install --color always -D ',
+    )
+    const child = exec(installCmd)
+    child.stdout.pipe(ui.log)
+    child.stderr.pipe(process.stderr)
+  })
+}
