4db0526b666ff26b1ea05d8bedaa03fdc722f37f
Ratson <github@ratson.name>
2016-12-22 00:39:48 +0800

    Generate config

--- tools/gen-concise-react.js
+++ tools/gen-concise-react.js
@@ -1,17 +1,21 @@
 import _ from 'lodash'
-import getRuleFinder from 'eslint-find-rules'
+import xoReact from 'eslint-config-xo-react'
 import airbnbReact from 'eslint-config-airbnb/rules/react'
 
 import {writeJsFile} from './gen-concise-style'
 
 export default () => {
-  const xoReact = getRuleFinder(require.resolve('eslint-config-xo-react/space'))
-
-  const config = {
+  const config = Object.assign({}, xoReact, {
     rules: Object.assign(
-      xoReact.getCurrentRulesDetailed(),
-      _.pick(airbnbReact.rules, ['react/jsx-closing-bracket-location']),
+      {},
+      airbnbReact.rules,
+      xoReact.rules,
+      _.pick(airbnbReact.rules, [
+        'react/jsx-closing-bracket-location',
+        'react/jsx-indent-props',
+        'react/jsx-indent',
+      ]),
     ),
-  }
+  })
   return writeJsFile('packages/eslint-config-concise-react/src/eslintrc.json', config)
 }
