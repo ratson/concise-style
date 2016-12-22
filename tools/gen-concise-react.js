86154feb73815f2f8f2f11b1aa4b28e21880c87b
Ratson <github@ratson.name>
2016-12-22 11:12:23 +0800

    Prettify ESLint rules

--- tools/gen-concise-react.js
+++ tools/gen-concise-react.js
@@ -4,13 +4,31 @@ import airbnbReact from 'eslint-config-airbnb/rules/react'
 
 import {writeJsFile} from './gen-concise-style'
 
+function prettifyRule(ruleValue) {
+  if (Array.isArray(ruleValue)) {
+    return [
+      prettifyRule(ruleValue[0]),
+      ..._.drop(ruleValue),
+    ]
+  }
+  switch (ruleValue) {
+    case 0:
+      return 'off'
+    case 2:
+      return 'error'
+    default:
+  }
+  return ruleValue
+}
+
 export default () => {
   const config = Object.assign({}, xoReact, {
     rules: Object.assign(
       {},
       airbnbReact.rules,
-      xoReact.rules,
+      _.mapValues(xoReact.rules, prettifyRule),
       _.pick(airbnbReact.rules, [
+        'react/jsx-boolean-value',
         'react/jsx-closing-bracket-location',
         'react/jsx-indent-props',
         'react/jsx-indent',
