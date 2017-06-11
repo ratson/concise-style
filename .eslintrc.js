eb369204cd94ed0ce450a099d6f0ed78c4b00449
Ratson <github@ratson.name>
2017-06-12 03:47:06 +0800

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports = {
+  extends: require.resolve('./packages/eslint-config-concise'),
+}
--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,7 +1,5 @@
 'use strict'
 
 module.exports = {
-  extends: [
-    require.resolve('../packages/eslint-config-concise-esnext/index.js'),
-  ],
+  extends: require.resolve('../packages/eslint-config-concise-esnext'),
 }
