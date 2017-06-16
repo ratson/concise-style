0df5c3f942d86585ddc1aaf75ba6be26ca72ad7c
Ratson <github@ratson.name>
2017-06-16 23:13:06 +0800

    Rewrite eslint-config-control-freak without using babel

---
+++ packages/eslint-config-control-freak/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { rules } = require('eslint-config-airbnb-base/rules/imports')
+
+const noExtraneousDependenciesOptions =
+  rules['import/no-extraneous-dependencies'][1]
+noExtraneousDependenciesOptions.devDependencies.push('**/*.spec.js')
+noExtraneousDependenciesOptions.optionalDependencies = true
+
+module.exports = {
+  extends: [
+    'eslint-config-airbnb-base',
+    'eslint-config-xo/esnext',
+    'eslint-config-concise',
+  ].map(require.resolve),
+  plugins: ['filenames', 'promise'],
+  rules: {
+    'filenames/match-exported': 'error',
+    'import/no-extraneous-dependencies': [
+      'error',
+      noExtraneousDependenciesOptions,
+    ],
+  },
+}
