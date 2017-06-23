a2777a44552f630c4ffd4cf4a7fbf8ed89932ecb
Ratson <github@ratson.name>
2017-06-23 23:51:42 +0800

    Add csc test

---
+++ test/csc.js
@@ -0,0 +1,11 @@
+import path from 'path'
+
+import execa from 'execa'
+import test from 'ava'
+
+test('code should pass csc lint', async t => {
+  const result = await execa(require.resolve('../packages/csc/lib/cli'), {
+    cwd: path.join(__dirname, '..'),
+  })
+  t.is(result.code, 0)
+})
