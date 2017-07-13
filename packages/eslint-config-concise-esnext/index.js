e79aaa705b92cb48ce5cc96036fde8b85fb3b007
Ratson <github@ratson.name>
2017-07-14 00:03:06 +0800

    Disable strict rule for esnext

--- packages/eslint-config-concise-esnext/index.js
+++ packages/eslint-config-concise-esnext/index.js
@@ -4,6 +4,7 @@ module.exports = {
   parser: require.resolve('babel-eslint'),
   plugins: ['babel'],
   rules: {
+    strict: 'off',
     'object-curly-spacing': 'off',
     'babel/object-curly-spacing': ['error', 'always'],
   },
