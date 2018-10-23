e5044f6750db573d240897c53371276ee71fb05a
Ratson <github@ratson.name>
2018-10-23 23:45:45 +0800

    Update neutrino-preset-concise for latest neutrino

--- packages/neutrino-preset-concise/index.js
+++ packages/neutrino-preset-concise/index.js
@@ -1,23 +1,20 @@
 'use strict'
 
 const lint = require('@neutrinojs/eslint')
+const { merge: eslintMerge } = require('eslint/lib/config/config-ops')
 
-module.exports = (neutrino, opts = {}) => {
-  neutrino.use(
-    lint,
-    lint.merge(
-      {
-        eslint: {
-          baseConfig: {
-            extends: [
-              require.resolve('eslint-config-concise'),
-              require.resolve('eslint-config-concise-esnext'),
-              require.resolve('eslint-config-concise-react'),
-            ],
-          },
-        },
-      },
-      opts,
-    ),
-  )
+module.exports = (neutrino, { eslint = {}, ...opts } = {}) => {
+  neutrino.use(lint, {
+    ...opts,
+    eslint: {
+      ...eslint,
+      baseConfig: eslintMerge({
+        extends: [
+          require.resolve('eslint-config-concise'),
+          require.resolve('eslint-config-concise-esnext'),
+          require.resolve('eslint-config-concise-react'),
+        ],
+      }),
+    },
+  })
 }
